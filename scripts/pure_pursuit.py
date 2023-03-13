4f2eee9b5e7dff99d175a2decfe864f11049e09a
Ibrahim Sel <ibrahim.sel58@hotmail.com>
2023-03-13 17:50:26 +0300

    pure_pursuit visualization works

--- scripts/pure_pursuit.py
+++ scripts/pure_pursuit.py
@@ -1,31 +1,99 @@
 #!/usr/bin/python3
 import rospy
-from geometry_msgs.msg import PoseStamped
 from sensor_msgs.msg import LaserScan
+from geometry_msgs.msg import TwistStamped, Pose, Point, Vector3, Quaternion, PoseStamped
+from std_msgs.msg import Header, ColorRGBA, String
+import numpy as np
+from ackermann_msgs.msg import AckermannDriveStamped
+import os
+from visualization_msgs.msg import Marker, MarkerArray
 
-# TODO: import ROS msg types and libraries
+LOOKAHEAD_DISTANCE = 0.5
+
+waypoints_file = open(
+    (os.path.expanduser('~')+'/rcws/logs/wp-2023-03-13-14-35-37.csv'), 'r')
+
+ct = 0
 
 class PurePursuit(object):
-    """
-    The class that handles pure pursuit.
-    """
     def __init__(self):
-        # TODO: create ROS subscribers and publishers.
+        self.pose_sub = rospy.Subscriber(
+            '/gt_pose', PoseStamped, self.pose_callback)
+        self.scan_sub = rospy.Subscriber(
+            '/scan', LaserScan, self.scan_callback)
+        self.steering_pub = rospy.Publisher(
+            '/drive', AckermannDriveStamped, queue_size=10)
+        self.visualize_pub = rospy.Publisher('visualizing', MarkerArray, queue_size=10)
+        self.marker_array = MarkerArray()
+        
+
+    def scan_callback(self, scan_msg):
         pass
+        
+        
+    def visualize_waypoints(self):
+        pass
+
     def pose_callback(self, pose_msg):
-        # TODO: find the current waypoint to track using methods mentioned in lecture
+        global ct
+        self.pose = pose_msg
+        self.x = pose_msg.pose.position.x
+        self.y = pose_msg.pose.position.y
+        self.theta = pose_msg.pose.orientation.z
+        contents = waypoints_file.read()
+        lines = contents.splitlines()
+        for i in lines:
+            ct += 1
+            splitted = i.split(",")
+            x = float(splitted[0])
+            y = float(splitted[1])
+            # angle = 
+            # speed = 
+            # self.publish_steering_angle(speed, angle)
+            # print(f"x: {x}, y: {y}")
+            marker = Marker()
+            marker.id = ct
+            marker.header.frame_id = "map"
+            marker.type = Marker.SPHERE
+            marker.action = Marker.ADD
+            marker.pose.position.x = x
+            marker.pose.position.y = y
+            marker.pose.position.z = 0
+            marker.pose.orientation.x = 0.0
+            marker.pose.orientation.y = 0.0
+            marker.pose.orientation.z = 0.0
+            marker.pose.orientation.w = 1.0
+            marker.scale.x = 0.1
+            marker.scale.y = 0.1
+            marker.scale.z = 0.1
+            marker.color.a = 1.0
+            marker.color.r = 1.0
+            marker.color.g = 0.0
+            marker.color.b = 0.0
+            self.marker_array.markers.append(marker)
+        self.visualize_pub.publish(self.marker_array)
 
-        # TODO: transform goal point to vehicle frame of reference
+    def calculate_steering_angle(self, lookahead_point_x, lookahead_point_y, x, y):
+        dx = lookahead_point_x - x
+        dy = lookahead_point_y - y
+        return np.arctan2(dy, dx)
 
-        # TODO: calculate curvature/steering angle
-
-        # TODO: publish drive message, don't forget to limit the steering angle between -0.4189 and 0.4189 radians
-        pass
+    def publish_steering_angle(self, speed, angle):
+        msg = AckermannDriveStamped()
+        msg.header.stamp = rospy.Time.now()
+        msg.header.frame_id = "pure_pursuit"
+        msg.drive.steering_angle = angle
+        msg.drive.speed = speed
+        self.steering_pub.publish(msg)
 
 
 def main():
     rospy.init_node('pure_pursuit_node')
     pp = PurePursuit()
     rospy.spin()
+    waypoints_file.close()
+
+
+
 if __name__ == '__main__':
     main()
--- scripts/waypoint_logger.py
+++ scripts/waypoint_logger.py
@@ -3,30 +3,45 @@ import rospy
 import numpy as np
 import atexit
 import tf
+import time
 from os.path import expanduser
 from time import gmtime, strftime
 from numpy import linalg as LA
 from tf.transformations import euler_from_quaternion
 from nav_msgs.msg import Odometry
+import math
+
 
 home = expanduser('~')
 file = open(strftime(home+'/rcws/logs/wp-%Y-%m-%d-%H-%M-%S',gmtime())+'.csv', 'w')
+
+prev_waypoint = {'x': 0, 'y': 0}
 
 def save_waypoint(data):
     quaternion = np.array([data.pose.pose.orientation.x, 
                            data.pose.pose.orientation.y, 
                            data.pose.pose.orientation.z, 
                            data.pose.pose.orientation.w])
+    
 
-    euler = tf.transformations.euler_from_quaternion(quaternion)
+    euler = euler_from_quaternion(quaternion)
     speed = LA.norm(np.array([data.twist.twist.linear.x, 
                               data.twist.twist.linear.y, 
                               data.twist.twist.linear.z]),2)
-    if data.twist.twist.linear.x > 0.:
-        print(data.twist.twist.linear.x)
-        print("here")
+    
+    dist = 0.0
+    dx = data.pose.pose.position.x - prev_waypoint['x']
+    dy = data.pose.pose.position.y - prev_waypoint['y']
+    dist = (math.pow(dx, 2) + math.pow(dy, 2))
+    if dist > 0.1:
+        file.write(f'{data.pose.pose.position.x}, {data.pose.pose.position.y}, {euler[2]}, {speed}\n')
+        prev_waypoint['x'] = data.pose.pose.position.x
+        prev_waypoint['y'] = data.pose.pose.position.y
 
-    file.write(f'{data.pose.pose.position.x}, {data.pose.pose.position.y}, {euler[2]}, {speed}\n')
+    # if data.twist.twist.linear.x > 0.:
+        # print(f'x: {data.pose.pose.position.x}, y: {data.pose.pose.position.y}, euler: {euler[2]}, speed: {speed}\n')
+    
+
 
 def shutdown():
     file.close()
@@ -34,7 +49,7 @@ def shutdown():
  
 def listener():
     rospy.init_node('waypoints_logger', anonymous=True)
-    rospy.Subscriber('pf/pose/odom', Odometry, save_waypoint)
+    rospy.Subscriber('odom', Odometry, save_waypoint)
     rospy.spin()
 
 if __name__ == '__main__':
