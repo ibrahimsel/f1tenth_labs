d7261c2d3bafbc0ce68f8225abc216295b9806d3
Ibrahim Sel <ibrahim.sel58@hotmail.com>
2023-03-14 18:03:14 +0300

    goal_point visualization works

--- scripts/pure_pursuit.py
+++ scripts/pure_pursuit.py
@@ -7,9 +7,8 @@ import rospy
 import numpy as np
 import os
 import math
-import uuid
 
-LOOKAHEAD_DISTANCE = 0.5
+LOOKAHEAD_DISTANCE = 1.0
 waypoints_file = open(
     (os.path.expanduser('~')+'/rcws/logs/waypoints_file.csv'), 'r')
 ct = 0  # for marker ids
@@ -59,7 +58,7 @@ class PurePursuit(object):
 
     def get_closest_waypoint(self, car_point):
         min_distance = np.inf
-        closest_point = self.waypoints[13]
+        closest_point = self.waypoints[0]
         for i in self.waypoints:
             if math.dist(car_point, i) < min_distance:
                 closest_point = i
@@ -86,17 +85,56 @@ class PurePursuit(object):
         self.visualize_pub_marker.publish(marker)
         return closest_point
     
+    def get_goal_waypoint(self, car_point, closest_waypoint):
+        goal_waypoint = []
+        furthest_distance = -np.inf
+# 
+        for i in self.waypoints:
+            distance = math.dist(car_point, i)
+            if (distance <= LOOKAHEAD_DISTANCE) and (self.waypoints.index(i) > self.waypoints.index(closest_waypoint)):
+                if (distance > furthest_distance):
+                    furthest_distance = distance
+                    goal_waypoint = i
+        
+        try:
+            marker = Marker()
+            marker.id = -2
+            marker.header.frame_id = "map"
+            marker.type = Marker.CUBE
+            marker.action = Marker.ADD
+            marker.pose.position.x = goal_waypoint[1]
+            marker.pose.position.y = goal_waypoint[0]
+            marker.pose.position.z = 0
+            marker.pose.orientation.x = 0.0
+            marker.pose.orientation.y = 0.0
+            marker.pose.orientation.z = 0.0
+            marker.pose.orientation.w = 1.0
+            marker.scale.x = 0.1
+            marker.scale.y = 0.1
+            marker.scale.z = 0.1
+            marker.color.a = 1.0
+            marker.color.r = 0.0
+            marker.color.g = 1.0
+            marker.color.b = 1.0
+            self.visualize_pub_marker.publish(marker)
+            return goal_waypoint
+        except IndexError:
+            print("err")
+    
     def pose_callback(self, pose_msg):
         x = pose_msg.pose.position.x
         y = pose_msg.pose.position.y
         theta = pose_msg.pose.orientation.z
         closest_waypoint = self.get_closest_waypoint([y, x])
-        angle = self.calculate_steering_angle(LOOKAHEAD_DISTANCE, closest_waypoint[0] - y)
+        goal_waypoint = self.get_goal_waypoint([y, x], closest_waypoint)
+        angle = self.calculate_steering_angle(LOOKAHEAD_DISTANCE, abs(goal_waypoint[1] - x))
+        angle = math.radians(angle)
+        print(angle)
         self.visualize_pub_markerarray.publish(self.marker_array)
-        # self.publish_steering(0.3, angle)
+        self.publish_steering(0.8, angle)
         
     def calculate_steering_angle(self, L, y):
-        return ((2*y) / (L**2))
+        return ((2*y)) / (math.pow(L, 2))
 
     def publish_steering(self, speed, angle):
         msg = AckermannDriveStamped()
